8d1919f9aaccd2cb9219252a085fa4e5962686c8
crchrist <codychrist@hotmail.com>
2021-06-30 12:17:13 -0700

    update HeroSection

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,9 +7,8 @@ function HeroSection() {
   return (
     <div className='hero-container'>
       <video src='/videos/video-1.mp4' autoPlay loop muted />
-      <h1>Unplanned Place</h1>
-      <h3>Find Your</h3>
-
+      <h1>Find Your</h1>
+      <p>Unplanned Place</p>
       <div className='hero-btns'>
         <Button
           className='btns'
